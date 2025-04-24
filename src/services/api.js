67115b1f2d783c80a697e4fa60a7868e0e70177c
Samarth <samarthhegde93@gmail.com>
2025-04-24 21:56:03 +0530

    frontned api2  update

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,10 @@
 // src/services/api.js
 import axios from 'axios';
 
+const API_BASE_URL = 'https://myplan-1.onrender.com/api';
 
 const api = axios.create({
-  baseURL: VITE_API_URL,
+  baseURL: API_BASE_URL,
   withCredentials: true,
 });
 
